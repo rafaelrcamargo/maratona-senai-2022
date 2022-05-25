6253f79e0a9114c2fdfb83b406f37dc0ba0b104b
RafaelRCamargo <rafaelrakochinski@gmail.com>
2022-05-25 20:27:25 -0300

    updates desafio_1

--- desafio_1.py
+++ desafio_1.py
@@ -1,10 +1,6 @@
-"""
-Desafio 1 - Soma de sub-vetor
-"""
+"""Desafio 1 - Soma de sub-vetor"""
 arr = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
-
 MAX = 0
-
 i = 0
 while i < len(arr):
     ii = i
@@ -15,5 +11,4 @@ while i < len(arr):
     if SUM > MAX:
         MAX = SUM
     i += 1
-
 print(MAX)
