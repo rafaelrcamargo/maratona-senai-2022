de05aca5013835b638aa9a261f75d7cb9ff79dd0
RafaelRCamargo <rafaelrakochinski@gmail.com>
2022-05-25 21:38:20 -0300

    updates desafio_3

--- desafio_3.py
+++ desafio_3.py
@@ -3,8 +3,33 @@ Desafio 3 - Elementar!
 """
 T = int(input())
 
-i = 1
-while i <= T:
+MAIN_COUNT = []
+
+I = 0
+while I < T:
     N = int(input())
-    
-    i += 1
+    SEQ = list(filter(None, input().split(" ")))
+    split = [int(x) for x in SEQ]
+
+    COUNT = 0
+
+    II = 0
+    while II < len(split):
+        NEW = II - 1
+        OLD = II
+
+        while split[OLD] < split[NEW] and NEW > -1:
+            split.insert(NEW, split.pop(OLD))
+            OLD = NEW
+            NEW = NEW - 1
+            COUNT += 1
+
+        II += 1
+
+    MAIN_COUNT.append(COUNT)
+
+    I += 1
+
+print()
+for count in MAIN_COUNT:
+    print(count)
